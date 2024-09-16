6844850765a3fe43b87f75d4c19704898cdab28e
avhixorin <avhixorin@gmail.com>
2024-09-16 15:23:03 +0530

    Refactor guess the number game UI and styles

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 let prevGuess = 0;
 let remGuess = 10;
-document.querySelector('#remGno').innerHTML = remGuess;
+let actNum = randomGen(); 
+
 
 function randomGen() {
-    return Math.floor(Math.random() * 100 + 1);
+    return Math.floor(Math.random() * 100) + 1;
 }
 
-let actNum = randomGen();
 
 function checkGameOver() {
     if (remGuess === 0) {
@@ -15,6 +15,7 @@ function checkGameOver() {
     }
 }
 
+
 function reset() {
     document.querySelector('#result').innerHTML = '';
     document.querySelector('#textArea').value = '';
@@ -22,32 +23,34 @@ function reset() {
     document.querySelector('#remGno').innerHTML = remGuess;
     document.querySelector('#prevGno').innerHTML = '';
     document.querySelector('#submit').disabled = false;
-    actNum = randomGen();
+    actNum = randomGen(); 
 }
+
 
 document.querySelector('#submit').addEventListener('click', () => {
     if (remGuess > 0) {
-        remGuess--;
-        document.querySelector('#remGno').innerHTML = remGuess;
-
         let userNum = parseInt(document.querySelector('#textArea').value, 10);
 
         if (isNaN(userNum) || userNum < 1 || userNum > 100) {
-            document.querySelector('#result').innerHTML = `Please enter a valid number between 1 and 100`;
+            document.querySelector('#result').innerHTML = 'Please enter a valid number between 1 and 100';
         } else {
-            if (userNum ===actNum) {
-                document.querySelector('#result').innerHTML = `Congratulations!!! You guessed it right`;
+            if (userNum === actNum) {
+                document.querySelector('#result').innerHTML = 'Congratulations!!! You guessed it right';
                 document.querySelector('#submit').disabled = true;
             } else {
-                document.querySelector('#result').innerHTML = `Sorry, try again`;
+                document.querySelector('#result').innerHTML = 'Sorry, try again';
+                document.querySelector('#textArea').value = '';
                 document.querySelector('#prevGno').innerHTML = userNum;
                 prevGuess = userNum;
+                remGuess--;
+                document.querySelector('#remGno').innerHTML = remGuess;
             }
         }
 
         checkGameOver();
     }
 });
+
 
 document.querySelector('#reset').addEventListener('click', () => {
     reset();
